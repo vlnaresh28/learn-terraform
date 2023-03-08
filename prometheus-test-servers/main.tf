c8898d97e92bfa65223baaaf8a4ed8eb53be0e7e
vlnaresh28 <vlnareshreddy@gmail.com>
2023-03-08 08:01:06 +0000

    d

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -2,17 +2,17 @@ resource "aws_spot_instance_request" "node1" {
   ami           = "ami-04af47c425b1eda74"
   instance_type = "t3.micro"
   vpc_security_group_id = ["sg-0d0c823795db46c70"]
-  wait_for_fullfillment= true
+  wait_for_fulfillment= true
   
   tags = {
     Name = "prom-test-server"
   }
 }
 
-resource "aws_spot_instance_request" "node2" {
+resource "aws_spot_instance_request" "node2 " {
   ami           = "ami-04af47c425b1eda74"
   instance_type = "t3.micro"
-  vpc_security_group_id = ["sg-0d0c823795db46c70"]
+  vpc_security_group_ids = ["sg-0d0c823795db46c70"]
   
   
   tags = {
