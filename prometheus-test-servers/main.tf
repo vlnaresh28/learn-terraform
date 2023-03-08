368660f288113062d4e351d80efa4c9deb78814d
vlnaresh28 <vlnareshreddy@gmail.com>
2023-03-08 07:54:22 +0000

    tee

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -9,7 +9,8 @@ resource "aws_spot_instance_request" "node1" {
   }
 }
 
-resource "aws_spot_instance_request" "node2" {
+resource "aws_spot_instance_request" "node2
+" {
   ami           = "ami-04af47c425b1eda74"
   instance_type = "t3.micro"
   vpc_security_group_id = ["sg-0d0c823795db46c70"]
