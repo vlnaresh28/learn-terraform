b645b0d3462d21aec1f9075b68553430aec2e2dc
vlnaresh28 <vlnareshreddy@gmail.com>
2023-03-08 08:27:56 +0000

    as

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -9,6 +9,12 @@ resource "aws_spot_instance_request" "node1" {
   }
 }
 
+resource "aws_ec2_tag" "node1" {
+  resource_id = aws_spot_instance_request.node1.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-server"
+}
+
 resource "aws_spot_instance_request" "node2" {
   ami           = "ami-04af47c425b1eda74"
   instance_type = "t3.micro"
@@ -18,4 +24,10 @@ resource "aws_spot_instance_request" "node2" {
   tags = {
     Name = "prom-test-server"
   }
+}
+
+resource "aws_ec2_tag" "node2" {
+  resource_id = aws_spot_instance_request.node2.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-node"
 }
