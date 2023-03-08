8fbe3ab0d62175d3b2dda580fcd29907eb44cc81
vlnaresh28 <vlnareshreddy@gmail.com>
2023-03-08 08:03:51 +0000

    ba

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -1,7 +1,7 @@
 resource "aws_spot_instance_request" "node1" {
   ami           = "ami-04af47c425b1eda74"
   instance_type = "t3.micro"
-  vpc_security_group_id = ["sg-0d0c823795db46c70"]
+  vpc_security_group_ids = ["sg-0d0c823795db46c70"]
   wait_for_fulfillment= true
   
   tags = {
