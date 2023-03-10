383f6d1887d2501c42ffff0ac091d247dbff133a
vlnaresh28 <vlnareshreddy@gmail.com>
2023-03-10 18:11:57 +0000

    id

--- roboshop-setup/ex1/servers.tf
+++ roboshop-setup/ex1/servers.tf
@@ -1,3 +1,21 @@
+resource "aws_instance" "frontend" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0d0c823795db46c70"]
+  tags = {
+    Name = "frontend"
+  }
+}
+
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z020734418XXZIMNTHHTZ"
+  name    = "frontend-dev.learndevopseasy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
+
+
 resource "aws_instance" "cart" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
@@ -6,6 +24,15 @@ resource "aws_instance" "cart" {
     Name = "cart"
   }
 }
+
+resource "aws_route53_record" "cart" {
+  zone_id = "Z020734418XXZIMNTHHTZ"
+  name    = "cart-dev.learndevopseasy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.cart.private_ip]
+}
+
 
 resource "aws_instance" "catalogue" {
   ami                    = "ami-0a017d8ceb274537d"
@@ -16,6 +43,16 @@ resource "aws_instance" "catalogue" {
   }
 }
 
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z020734418XXZIMNTHHTZ"
+  name    = "catalogue-dev.learndevopseasy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.catalogue.private_ip]
+}
+
+
+
 resource "aws_instance" "user" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
@@ -24,6 +61,15 @@ resource "aws_instance" "user" {
     Name = "user"
   }
 }
+
+resource "aws_route53_record" "user" {
+  zone_id = "Z020734418XXZIMNTHHTZ"
+  name    = "user-dev.learndevopseasy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.user.private_ip]
+}
+
 
 resource "aws_instance" "shipping" {
   ami                    = "ami-0a017d8ceb274537d"
@@ -34,6 +80,15 @@ resource "aws_instance" "shipping" {
   }
 }
 
+resource "aws_route53_record" "shipping" {
+  zone_id = "Z020734418XXZIMNTHHTZ"
+  name    = "shipping-dev.learndevopseasy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.shipping.private_ip]
+}
+
+
 resource "aws_instance" "payment" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
@@ -41,6 +96,14 @@ resource "aws_instance" "payment" {
   tags = {
     Name = "payment"
   }
+}
+
+resource "aws_route53_record" "payment" {
+  zone_id = "Z020734418XXZIMNTHHTZ"
+  name    = "payment-dev.learndevopseasy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.payment.private_ip]
 }
 
 resource "aws_instance" "mongodb" {
@@ -52,6 +115,16 @@ resource "aws_instance" "mongodb" {
   }
 }
 
+resource "aws_route53_record" "mongodb" {
+  zone_id = "Z020734418XXZIMNTHHTZ"
+  name    = "mongodb-dev.learndevopseasy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mongodb.private_ip]
+}
+
+
+
 resource "aws_instance" "mysql" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
@@ -60,6 +133,16 @@ resource "aws_instance" "mysql" {
     Name = "mysql"
   }
 }
+
+resource "aws_route53_record" "mysql" {
+  zone_id = "Z020734418XXZIMNTHHTZ"
+  name    = "mysql-dev.learndevopseasy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.mysql.private_ip]
+}
+
+
 
 resource "aws_instance" "redis" {
   ami                    = "ami-0a017d8ceb274537d"
@@ -70,12 +153,28 @@ resource "aws_instance" "redis" {
   }
 }
 
-resource "aws_instance" "RabbitMq" {
+
+resource "aws_route53_record" "redis" {
+  zone_id = "Z020734418XXZIMNTHHTZ"
+  name    = "redis-dev.learndevopseasy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.redis.private_ip]
+}
+
+resource "aws_instance" "rabbitmq" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0d0c823795db46c70"]
   tags = {
-    Name = "RabbitMQ"
+    Name = "rabbitmQ"
   }
 }
 
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "Z020734418XXZIMNTHHTZ"
+  name    = "rabbitmq-dev.learndevopseasy.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.rabbitmq.private_ip]
+}
