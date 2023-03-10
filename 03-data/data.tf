cfc8b0078e6c27b62975401e9369fc5d43c88412
vlnaresh28 <vlnareshreddy@gmail.com>
2023-03-10 11:46:12 +0000

    datte

--- 03-data/data.tf
+++ 03-data/data.tf
@@ -1,6 +1,11 @@
-data "aws_ami" "local_label_by_user-ami" {
+data "aws_ami" "ami" {
 
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["973714476881"]
+}
 
+output "ami_id" {
+    
+    value = data.aws_ami.ami.image_id
+}
