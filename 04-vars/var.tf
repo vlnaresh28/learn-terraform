5daaa1af43a3440600686e3dc9dec44fef16ee82
vlnaresh28 <vlnareshreddy@gmail.com>
2023-03-10 17:19:10 +0000

    v

--- 04-vars/var.tf
+++ 04-vars/var.tf
@@ -17,3 +17,37 @@ variable "cli" {}
 output "printing_the_CLI_variable" {
     value= var.cli
     }
+    
+variable "input" {}
+
+variable "sample5" {
+    
+    default = "Hello"
+    
+    }
+    
+
+variable "sample6" {
+    
+    default = [
+      "Hello",
+      1000,
+      true,
+      "world"
+    ]
+    }
+    
+variable "sample7" {
+    
+    default = {
+      string = "Hello",
+      number = 1000,
+      boolean = true,
+      
+    ]
+    }
+output "types"{
+    
+    value = "variable sample5 ${var.sample5}, First value in the list ${var.sample6[0]}, Boolean value of the MAP ${var.sample7[boolean]} "
+}    
+    
