17ccf8f96ff2ee01db5834de2aa7c875770b3655
vlnaresh28 <vlnareshreddy@gmail.com>
2023-03-10 10:42:30 +0000

    g

---
+++ 01-sample/test.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "ec2" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0d0c823795db46c70"]
+  tags = {
+    Name = "test"
+  }
+}
