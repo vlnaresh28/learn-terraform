d1379affd5687675b8c59d721c54bd7702dd32d9
vlnaresh28 <vlnareshreddy@gmail.com>
2023-03-11 09:15:41 +0000

    dev

--- roboshop-setup/ex4/ec2/main.tf
+++ roboshop-setup/ex4/ec2/main.tf
@@ -8,7 +8,7 @@ data "aws_ami" "ami" {
 resource "aws_instance" "ec2" {
   ami                    = data.aws_ami.ami.image_id
   instance_type          = var.instance_type
-  vpc_security_group_ids = [var.sg_id]
+  vpc_security_group_ids = ["sg-0d0c823795db46c70"]
   tags = {
     Name = var.component
   }
--- roboshop-setup/ex4/main.tf
+++ roboshop-setup/ex4/main.tf
@@ -3,12 +3,10 @@ module "ec2" {
   source        = "./ec2"
   component     = each.value["name"]
   instance_type = each.value["type"]
-  sg_id         = module.sg.sg_id
+
 }
 
-module "sg" {
-  source = "./sg"
-}
+
 
 
 variable "instances" {
