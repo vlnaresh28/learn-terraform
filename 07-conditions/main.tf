3e0cf36b9f40b472962a36daef119ba3aa23c151
vlnaresh28 <vlnareshreddy@gmail.com>
2023-03-11 07:23:24 +0000

    condition

--- 07-conditions/main.tf
+++ 07-conditions/main.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "test" {
     ami           = "ami-0a017d8ceb274537d"
-    instance_type = var.instance_type
+    instance_type = var.instance_type == "" ? "t3.micro"
+    
 }
 
 variable "instance_type" {}
